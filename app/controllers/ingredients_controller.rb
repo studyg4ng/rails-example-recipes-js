436ded3bc9e83ce6b75532f8dc8b74264b81c447
Brigitte Jellinek <brigitte.jellinek@fh-salzburg.ac.at>
2015-11-27 08:05:47 +0100

    more todo

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -42,7 +42,7 @@ class IngredientsController < ApplicationController
   def update
     respond_to do |format|
       if @ingredient.update(ingredient_params)
-        format.html { redirect_to @ingredient, notice: 'Ingredient was successfully updated.' }
+        format.html { redirect_to ingredients_path, notice: 'Ingredient was successfully updated.' }
         format.json { render :show, status: :ok, location: @ingredient }
       else
         format.html { render :edit }
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # translate flash types (success, error, alert, notice) 
+  # to bootstrap class names
+  def bootstrap_class_for flash_type
+    { "success" => "alert-success", 
+      "error"   => "alert-danger", 
+      "alert"   => "alert-warning", 
+      "notice"  => "alert-info"      }[flash_type] || "nope"
+  end
+
 end
