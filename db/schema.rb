eae0d662c11869283cf3756e2b2c02d21367a8e6
Brigitte Jellinek <brigitte.jellinek@fh-salzburg.ac.at>
2017-01-06 14:01:45 +0100

    added two images

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -20,10 +19,9 @@ ActiveRecord::Schema.define(version: 20151125230237) do
     t.string   "unit"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.index ["ingredient_id"], name: "index_contains_on_ingredient_id"
+    t.index ["recipe_id"], name: "index_contains_on_recipe_id"
   end
-
-  add_index "contains", ["ingredient_id"], name: "index_contains_on_ingredient_id"
-  add_index "contains", ["recipe_id"], name: "index_contains_on_recipe_id"
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
